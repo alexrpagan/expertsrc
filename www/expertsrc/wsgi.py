57cbdc29b71201b24246fd67eadf34ab03d4f023
Alex Pagan <alexrpagan@gmail.com>
2012-09-19 14:22:12 -0400

    WSGI callable for deployment with a real web server

---
+++ www/expertsrc/wsgi.py
@@ -0,0 +1,10 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'expertsrc.settings'
+
+import django.core.handlers.wsgi
+
+application = django.core.handlers.wsgi.WSGIHandler()
+
