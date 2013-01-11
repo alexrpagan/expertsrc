e1762969fd25b8d2b45e91c6c96940a3aef77af7
Alex Pagan <alexrpagan@gmail.com>
2013-01-11 16:26:32 -0500

    Remove redis from initialization step

--- www/expertsrc/ui/management/commands/reset_nr_questions.py
+++ www/expertsrc/ui/management/commands/reset_nr_questions.py
@@ -2,22 +2,15 @@ from django.core.management.base import NoArgsCommand
 from ui.models import *
 from django.db import connections, transaction
 from django.conf import settings
-import redis
 
 class Command(NoArgsCommand):
     @transaction.commit_on_success
     def handle_noargs(self, **options):
-        cur = connections[settings.TAMER_DB].cursor()
-        print 'clear loaded data sources from remote database.'
-        cur.execute('select * from unload_local()')
-        cur.connection.commit()
+        #cur = connections[settings.TAMER_DB].cursor()
+        #print 'clear loaded data sources from remote database.'
+        #ur.execute('select * from unload_local()')
+        #cur.connection.commit()
 
-        print 'flush the message queue'
-        r = redis.Redis()
-        r.delete('question')
-        r.delete('answer')
-        r.delete('review')
-        
         print 'delete the guy who owns all of the schema mapping questions'
         User.objects.filter(username='data-tamer').delete()
 
@@ -26,5 +19,5 @@ class Command(NoArgsCommand):
         dt.save()
         profile = UserProfile(user=dt,
                               user_class='ASK',
-                              bank_balance=100000000)
+                              bank_balance=100000)
         profile.save()
